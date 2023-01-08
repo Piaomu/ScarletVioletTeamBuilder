cf9699b90e575970eeb8a9009c32a7134fc74784
piaomu <driftwood.wahl@gmail.com>
2023-01-08 07:17:22 -0600

    Update pokeball component to add pkmn image

--- src/app/shared/pokeball.component.ts
+++ src/app/shared/pokeball.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pokeball',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeballComponent implements OnInit {
   constructor() {}
+  @Input() imageUrl!: string | undefined;
 
   ngOnInit(): void {}
 }
